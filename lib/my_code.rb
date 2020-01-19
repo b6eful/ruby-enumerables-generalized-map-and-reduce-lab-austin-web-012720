16327b10e00aecfea9443dc9bb21ebb4150490b5
Phoenix R <phoenixrabang@gmail.com>
2020-01-19 20:15:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,2 +1,26 @@
 # Your Code Her
-def 
+def lit(array)
+  arrays = []
+  v = 0
+  while v < array.length
+    arrays.push(yield(array[i]))
+  v += 1
+  end
+  arrays
+end
+
+def subs(array,starting_value=nil)
+  if starting_value
+    sum = starting_value
+    i = 0
+  else
+    sum = array [0]
+    i = 1
+  end
+  
+  while i < array.length
+    sum = yield(sum,array[i])
+    i += 1
+  end
+  sum
+end
